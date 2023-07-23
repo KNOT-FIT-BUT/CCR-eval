9daab6c66ea0f3e9fe6eaade49d29df45d26d033
startler03 <jakub.stetina2003@gmail.com>
2023-07-23 12:07:41 +0200

    parse dareczech into jsonl format

--- dareczech.py
+++ dareczech.py
@@ -1,37 +1,93 @@
+import argparse
+import logging
+import json
 import os
 import re
+from tqdm import tqdm
 
 from lemmatize import Lemmatizer
 from dareczech_reg import DOC_TITLE_REG, DOC_URL_REG, DOC_BTE_REG
 
-INPUT_PATH = "dareczech/dev.tsv"
+logging.basicConfig(format='%(levelname)s: %(message)s')
 
-if not os.path.exists(INPUT_PATH):
-    raise FileNotFoundError
+def squash_doc_content(
+        title:str,
+        doc_title:str="",
+        doc_bte:str="", 
+        sep:str="\n") -> str:
+    return title + sep + doc_title + sep + doc_bte + sep
 
+# Default out directory
+OUT_DIR = "documents"
+
+# Parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-i", "--input", 
+    required=True,
+    action="store",
+    help="Input file",
+    dest="input_file"
+)
+
+parser.add_argument(
+    "-d", "--out-dir", 
+    required=False, 
+    action="store", 
+    help="Output directory",
+    dest="out_dir"
+)
+
+args = parser.parse_args()
+input_path = os.path.abspath(args.input_file)
+if not os.path.exists(input_path):
+    logging.error(f"Input file does not exist ({input_path})")
+    exit(1)
+
+input_lines_num = sum(1 for line in open(input_path)) - 1
+
+if args.out_dir:
+    OUT_DIR = args.out_dir
+
+
+if not input_path.endswith(".tsv"):
+    logging.warning("Input file might not be in the correct format (expected .tsv)")
+
+output_path = os.path.basename(os.path.abspath(input_path))
+output_path = f"{OUT_DIR}/{os.path.splitext(output_path)[0]}.jsonl"
+
+logging.info("Loading model")
 lemmatizer = Lemmatizer()
 lemmatizer.load_model()
 
-with open(INPUT_PATH) as file_in:
+with open(input_path) as file_in, open(output_path, "w") as file_out:
     # Skip first line (tsv header)
     next(file_in)
 
-    for line in file_in:
+    for line in tqdm(file_in, total=input_lines_num, desc="Processing", unit="line"):
         data = line.split("\t")
         
-        # id      = data[0]
+        id      = data[0]
         # query   = data[1]
         # url     = data[2]
         doc     = data[3]
-        # title   = data[4]
+        title   = data[4]
         # label   = data[5]
 
-        # doc_title   = re.search(DOC_TITLE_REG, doc).group(1)
+        doc_title   = re.search(DOC_TITLE_REG, doc).group(1)
         # doc_url     = re.search(DOC_URL_REG, doc).group(1)
         doc_bte     = re.search(DOC_BTE_REG, doc).group(1)
-        lemmatized = lemmatizer.lemmatize_text(doc_bte)
-        print(lemmatized)
-        break
+        doc_bte_lemm = lemmatizer.lemmatize_text(doc_bte)
 
-lemmatizer = Lemmatizer()
+        doc_content_squash = squash_doc_content(title=title, doc_title=doc_title, doc_bte=doc_bte)
+
+        # Pyserini expected format
+        doc_json = {
+            "id": id,
+            "contents" : doc_content_squash
+        }
+
+        file_out.write(json.dumps(doc_json, ensure_ascii=False) + "\n")
+
+logging.info("Done")
 
