b60501e39d339a4ea4eef699dd2b7d26db85d2cb
startler03 <jakub.stetina2003@gmail.com>
2023-08-13 08:40:59 +0200

    Change morphodita dirname

--- fetch_czech_morph_model.sh
+++ fetch_czech_morph_model.sh
@@ -1,7 +1,7 @@
-MODELS_DIR="models/"
+MORPHO_DIR="morphodita/"
 DWNLD_LINK='https://lindat.mff.cuni.cz/repository/xmlui/bitstream/handle/11234/1-4794{/czech-morfflex2.0-pdtc1.0-220710.zip}'
 
-mkdir -p "$MODELS_DIR" && cd "$MODELS_DIR"
+mkdir -p "$MORPHO_DIR" && cd "$MORPHO_DIR"
 curl --remote-name-all "$DWNLD_LINK"
 unzip *.zip
 rm *.zip
