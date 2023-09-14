026f1b0621c9794697288b2140c7a4e6da67e1f6
startler03 <jakub.stetina2003@gmail.com>
2023-09-14 12:42:48 +0200

    load_pairs function

--- utils/collection.py
+++ utils/collection.py
@@ -1,23 +1,11 @@
 from colbert.data import Collection
 import json
 
-def load_collection(collection_path:str):
-    doc_subset = []
-    with open(collection_path) as file:
-        for line in file:
-            line = json.loads(line)
-
-            doc_dict = {
-                "id": line['url'],
-                "cc_file": "cc_file",
-                "time": None,
-                "title": None,
-                "text": line['doc'],
-                "url": line['url']
-            }
-
-            doc_subset.append(doc_dict)
-
-    collection = Collection.cast(doc_subset)
-    return doc_subset, collection
+def load_pairs(id_url_pairs:str) ->dict:
+    with open(id_url_pairs) as pairs_file:
+        pairs = {}
+        for line in pairs_file:
+            id, url = line.rstrip().split("\t")
+            pairs[int(id)] = url
+    return pairs
 
